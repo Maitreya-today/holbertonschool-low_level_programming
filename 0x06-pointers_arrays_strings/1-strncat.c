85f18a3be3d9ff84ecfb17dc2cc2df20cfe6223f
Maitreya-today <lessismoreclint@gmail.com>
2022-09-30 15:26:56 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,31 @@
 #include "holberton.h"
+
 /**
- * *_strncat - concatenate two strings but do
- * not include null.
- *
+ * _strncat - prototype for two strings
  * @dest: s1
  * @src: s2
  * @n: number of bytes
- *
- * Return: 0;
+ * Return: dest
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
-	int a = 0;
+	int a;
 	int b;
 
+	a = 0;
 	while (dest[a] != '\0')
-		++a;
-	for (b = 0; b < n && src[b] != '\0'; b++)
-		dest[a + b] = '\0';
+	{
+		a++;
+	}
+
+	b = 0;
+	while (b < n && src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
 	return (dest);
 }
