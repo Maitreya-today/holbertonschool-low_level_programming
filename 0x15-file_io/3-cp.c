10e237714412445d1edb2fda4ac3c1e4f5e8ea08
Maitreya-today <lessismoreclint@gmail.com>
2022-11-09 21:58:19 -0800

    file input-output final

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#ifndef BUFF_SIZE
+#define BUFF_SIZE 1024
+#endif
+/**
+ * main - function with two arguments
+ * @argc: count of arguments
+ * @argv: variadic arguments
+ * Description: copies the content of a file to another file
+ * Return: 0 for success and exit codes for failure
+ */
+int main(int argc, char *argv[])
+{
+	int file_origin, file_dest;
+	ssize_t numb;
+	char buff[BUFF_SIZE];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	file_origin = open(argv[1], O_RDONLY);
+	if (file_origin == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	file_dest = open(argv[2], O_CREAT | O_WRONLY, 0664);
+	if (file_dest == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	while ((numb = read(file_origin, buff, BUFF_SIZE)) > 0)
+	{
+		if (write(file_dest, buff, numb) != numb)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (numb == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if ((close(file_origin) == -1) || (close(file_dest) == -1))
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_origin);
+		exit(100);
+	}
+	exit(EXIT_SUCCESS);
+}
